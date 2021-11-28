8de9a2210fdbfb2ab02fc8f7cd736f3af7bc8c68
Fernando de Francisco <f3rd3f@gmail.com>
2021-11-28 23:49:20 +0100

    Added Top Left and Right

--- RectTiler/WindowManager.cpp
+++ RectTiler/WindowManager.cpp
@@ -9,6 +9,8 @@ void RegisterKeys(HWND hWnd)
 {
     RegisterHotKey(hWnd, LEFT_HOTKEY_ID, 6, VK_LEFT);
     RegisterHotKey(hWnd, RIGHT_HOTKEY_ID, 6, VK_RIGHT);
+    RegisterHotKey(hWnd, TOP_LEFT_HOTKEY_ID, 7, VK_LEFT);
+    RegisterHotKey(hWnd, TOP_RIGHT_HOTKEY_ID, 7, VK_RIGHT);
 }
 
 void DeregisterEverything(void)
@@ -19,7 +21,6 @@ void DeregisterEverything(void)
 void PositionWindow(int option)
 {
     RECT rectScreen;
-    RECT rectWindow;
 
     HWND hWnd = GetForegroundWindow();
     HMONITOR monitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
@@ -28,22 +29,92 @@ void PositionWindow(int option)
 
     GetMonitorInfo(monitor, (LPMONITORINFO)&info);
     rectScreen = info.rcWork;
-    int screenWidth = rectScreen.right - rectScreen.left;
-    int screenHeight = rectScreen.bottom - rectScreen.top;
-
-    GetWindowRect(hWnd, &rectWindow);
-    int windowWidth = rectWindow.right - rectWindow.left;
-    int windowHeight = rectWindow.bottom - rectWindow.top;
+    
+    RECT windowRect;
 
     switch (option)
     {
     case LEFT_HOTKEY_ID:
-        SetWindowPos(hWnd, NULL, rectScreen.left, rectScreen.top, 2 * (screenWidth / 3), screenHeight, SWP_NOZORDER);
+        windowRect = GetWindowLeftTwoThird(rectScreen);
         break;
     case RIGHT_HOTKEY_ID:
-        SetWindowPos(hWnd, NULL, rectScreen.right - (2 * (screenWidth / 3)), rectScreen.top, 2 * (screenWidth / 3), screenHeight, SWP_NOZORDER);
+        windowRect = GetWindowRightTwoThird(rectScreen);
         break;
+    case TOP_LEFT_HOTKEY_ID:
+        windowRect = GetWindowTopLeftTwoThird(rectScreen);
+        break;
+    case TOP_RIGHT_HOTKEY_ID:
+        windowRect = GetWindowTopRightTwoThird(rectScreen);
+        break;
+    default:
+        return;
     }
+    SetWindowPos(hWnd, NULL, windowRect.left, windowRect.top, windowRect.right, windowRect.bottom, SWP_NOZORDER);
+}
+
+SIZE GetScreenSize(RECT rectScreen)
+{
+    SIZE size = {
+        rectScreen.right - rectScreen.left,
+        rectScreen.bottom - rectScreen.top
+    };
+    return size;
+}
+
+RECT GetWindowLeftTwoThird(RECT rectScreen)
+{
+    SIZE size = GetScreenSize(rectScreen);
+
+    RECT rect = {
+        rectScreen.left,
+        rectScreen.top,
+        2 * (size.cx / 3),
+        size.cy
+    };
+
+    return rect;
+}
+
+RECT GetWindowTopLeftTwoThird(RECT rectScreen)
+{
+    SIZE size = GetScreenSize(rectScreen);
+
+    RECT rect = {
+        rectScreen.left,
+        rectScreen.top,
+        2 * (size.cx / 3),
+        size.cy / 2
+    };
+
+    return rect;
+}
+
+RECT GetWindowRightTwoThird(RECT rectScreen)
+{
+    SIZE size = GetScreenSize(rectScreen);
+
+    RECT rect = {
+        rectScreen.right - (2 * (size.cx / 3)), 
+        rectScreen.top, 
+        2 * (size.cx / 3), 
+        size.cy
+    };
+
+    return rect;
+}
+
+RECT GetWindowTopRightTwoThird(RECT rectScreen)
+{
+    SIZE size = GetScreenSize(rectScreen);
+
+    RECT rect = {
+        rectScreen.right - (2 * (size.cx / 3)),
+        rectScreen.top,
+        2 * (size.cx / 3),
+        size.cy / 2
+    };
+
+    return rect;
 }
 
 void CreateSystemTray(HINSTANCE hInst, HWND hWnd)
--- RectTiler/WindowManager.h
+++ RectTiler/WindowManager.h
@@ -2,6 +2,8 @@
 
 #define LEFT_HOTKEY_ID 1
 #define RIGHT_HOTKEY_ID 2
+#define TOP_LEFT_HOTKEY_ID 3
+#define TOP_RIGHT_HOTKEY_ID 4
 #define MY_TRAY_ICON_ID 1
 #define MY_TRAY_ICON_MESSAGE 10
 
@@ -9,3 +11,7 @@ void RegisterKeys(HWND);
 void DeregisterEverything(void);
 void PositionWindow(int);
 void CreateSystemTray(HINSTANCE, HWND);
+RECT GetWindowLeftTwoThird(RECT rectScreen);
+RECT GetWindowTopLeftTwoThird(RECT rectScreen);
+RECT GetWindowTopRightTwoThird(RECT rectScreen);
+RECT GetWindowRightTwoThird(RECT rectScreen);
